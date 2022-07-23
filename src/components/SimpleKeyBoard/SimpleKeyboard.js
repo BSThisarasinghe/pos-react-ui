717a9414fa4c95b3b22a6b79dd3c9129090e8735
Buwaneka Sudheera <Buwaneka.Sudheera@dialog.lk>
2022-07-23 17:19:40 +0530

    Keyboard integration

--- src/components/SimpleKeyBoard/SimpleKeyboard.js
+++ src/components/SimpleKeyBoard/SimpleKeyboard.js
@@ -14,9 +14,80 @@ const SimpleKeyboard = ({ keyboardRef, inputName, layoutName, onChangeAll, onKey
                 onChangeAll={onChangeAll}
                 onKeyPress={onKeyPress}
                 onChange={onChangeInput}
+                layout={{
+                    default: [
+                        "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+                        "{tab} q w e r t y u i o p [ ] \\",
+                        "{lock} a s d f g h j k l ; ' {enter}",
+                        "{shift} z x c v b n m , . / {shift}",
+                        ".com @ {space} {downkeyboard}"
+                    ],
+                    shift: [
+                        "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
+                        "{tab} Q W E R T Y U I O P { } |",
+                        '{lock} A S D F G H J K L : " {enter}',
+                        "{shift} Z X C V B N M < > ? {shift}",
+                        ".com @ {space} {downkeyboard}"
+                    ]
+                }}
+                display={{
+                    "{downkeyboard}": "🞃",
+                    "{enter}": "return"
+                }}
+            // layout={{
+            //     default: [
+            //         "q w e r t y u i o p {bksp}",
+            //         "a s d f g h j k l {enter}",
+            //         "{shift} z x c v b n m , . {shift}",
+            //         "{alt} {space} {altright} {downkeyboard}"
+            //     ],
+            //     shift: [
+            //         "Q W E R T Y U I O P {bksp}",
+            //         "A S D F G H J K L {enter}",
+            //         "{shiftactivated} Z X C V B N M , . {shiftactivated}",
+            //         "{alt} {space} {altright} {downkeyboard}"
+            //     ],
+            //     alt: [
+            //         "1 2 3 4 5 6 7 8 9 0 {bksp}",
+            //         `@ # $ & * ( ) ' " {enter}`,
+            //         "{shift} % - + = / ; : ! ? {shift}",
+            //         "{default} {space} {back} {downkeyboard}"
+            //     ],
+            //     smileys: [
+            //         "😀 😊 😅 😂 🙂 😉 😍 😛 😠 😎 {bksp}",
+            //         `😏 😬 😭 😓 😱 😪 😬 😴 😯 {enter}`,
+            //         "😐 😇 🤣 😘 😚 😆 😡 😥 😓 🙄 {shift}",
+            //         "{default} {space} {altright} {downkeyboard}"
+            //     ]
+            // }}
+            // display={{
+            //     "{alt}": ".?123",
+            //     "{smileys}": "\uD83D\uDE03",
+            //     "{shift}": "⇧",
+            //     "{shiftactivated}": "⇧",
+            //     "{enter}": "return",
+            //     "{bksp}": "⌫",
+            //     "{altright}": ".?123",
+            //     "{downkeyboard}": "🞃",
+            //     "{space}": " ",
+            //     "{default}": "ABC",
+            //     "{back}": "⇦"
+            // }}
+            buttonTheme={[
+              {
+                class: "special__keys",
+                buttons: "{bksp} {enter} {alt} {smileys} {downkeyboard}"
+              },
+              {
+                class: "space__btn",
+                buttons: "{space}"
+              }
+            ]}
             />
         </React.Fragment>
     );
 }
+
+//#adb5bb
 
 export { SimpleKeyboard };
